51a6437693a20403015c91f7def88fc02915dca4
Sam Feder <sam.feder@amcnetworks.com>
2017-05-19 13:54:36 -0400

    remove userlist and add react-navigation

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppHeader from './components/AppHeader';
-import UserList from './components/UserList';
 
 export default class App extends React.Component {
   render() {
