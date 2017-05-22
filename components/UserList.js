4f9ea286e6771413152d316852a61832c7b59966
Sam Feder <sam.feder1@gmail.com>
2017-05-22 10:09:12 -0400

    attempt to configure eslint

--- components/UserList.js
+++ components/UserList.js
@@ -7,17 +7,35 @@ export default class ListThumbnailExample extends Component {
                 <Content>
                     <List>
                         <ListItem>
-                            <Thumbnail square size={80} source={require('../images/avatar_large.png')} />
+                            <Thumbnail
+                              square
+                              size={80}
+                              source={require('../images/avatar_large.png')} />
                             <Body>
-                                <Text>Sankhadeep</Text>
-                                <Text note>Its time to build a difference . .</Text>
+                                <Text>Shujon</Text>
+                                <Text note>AMC Networks</Text>
+                            </Body>
+                        </ListItem>
+                        <ListItem>
+                            <Thumbnail
+                              square
+                              size={80}
+                              source={require('../images/avatar_large.png')} />
+                            <Body>
+                                <Text>Fillbot 🤖</Text>
+                                <Text note>Romphim LLC</Text>
                             </Body>
                         </ListItem>
                         <ListItem>
                             <Thumbnail square size={80} source={require('../images/avatar_large.png')} />
                             <Body>
-                                <Text>Sankhadeep</Text>
-                                <Text note>Its time to build a difference . .</Text>
+                                <Text>Mom</Text>
+                            </Body>
+                        </ListItem>
+                        <ListItem>
+                            <Thumbnail square size={80} source={require('../images/avatar_large.png')} />
+                            <Body>
+                                <Text>Dad</Text>
                             </Body>
                         </ListItem>
                     </List>
